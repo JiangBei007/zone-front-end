fb83e133b900bc63605e9bbab022fea0e7f53475
jiangbei <engineersweb@163.com>
2019-03-19 17:51:57 +0800

    注册验证1

--- src/components/basic/register.js
+++ src/components/basic/register.js
@@ -1,24 +1,56 @@
 import React , { Component } from "react"
+import { message } from 'antd';
 import "./register.scss"
-
+import axios from "axios"
 
 export default class Register extends Component{
+	constructor(props){
+		super(props)
+		this.state = {
+			name:"",
+			SecretProtection:"",
+			password:"",
+			surepassword:"",
+		}
+	}
 	goSignin(){
 		this.props.history.push("/signin")
+	}
+	sure(){
+		const {name,SecretProtection,password,surepassword} = this.state;
+		if(!name){
+			return message.warn("请输入用户名",1);
+		}
+		if(!SecretProtection){
+			return message.warn("请输入密保字符串",1);
+		}
+		if(!password){
+			return message.warn("请输入密码",1);
+		}
+		if(!surepassword){
+			return message.warn("请确认密码",1);
+		}
 	}
 	render(){
 		return(<div id="register">
 				<div className="register">
-					<span></span>
 					<p className="p1">注册</p>
-					<input className="tel" type="tel" maxLength="11" placeholder="请输入正确的手机号码"/>
+					<input className="tel" type="text" maxLength="11" placeholder="请输入用户名" value={this.state.name} onChange={(e)=>{
+						this.setState({name:e.target.value})
+					}}/>
 					<div className="message">
-						<input className="text" type="text" maxLength="4" placeholder="请输入密保字符串"/>
+						<input className="text" type="text" value={this.state.SecretProtection} maxLength="64" placeholder="请输入密保字符串" onChange={(e)=>{
+							this.setState({SecretProtection:e.target.value})
+						}}/>
 						<span className="tips">查看</span>
 					</div>
-					<input className="passwordOne" type="password" placeholder="请输入密码"/>
-					<input className="passwordTwo" type="password" placeholder="请再次输入密码"/>
-					<button className="btn" >确认注册</button>
+					<input className="passwordOne" type="password"  maxLength="24" value={this.state.password} placeholder="请输入密码" onChange={(e)=>{
+						this.setState({password:e.target.value})
+					}}/>
+					<input className="passwordTwo" type="password" maxLength="24"  value={this.state.surepassword} placeholder="请再次输入密码" onChange={(e)=>{
+						this.setState({surepassword:e.target.value})
+					}}/>
+					<button className="btn" onClick={this.sure.bind(this)}>确认注册</button>
 					<p className="p3">已有账号，立即<span onClick={this.goSignin.bind(this)}>登录</span></p>
 				</div>
     </div>)
